dc1f8f4c6378a34372ae65c0ab3874c09739f3f9
SMMCoreDev <smmcoredev@protonmail.com>
2019-04-17 13:55:05 -0400

    Release 1.0.1

--- src/miner.cpp
+++ src/miner.cpp
@@ -555,9 +555,9 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
 				
             int64_t nReserved = max(nReserveBalance, nPos);
 
-            //while (chainActive.Tip()->nTime < 1471482000 || vNodes.empty() || pwallet->IsLocked() || !fMintableCoins || nReserved >= pwallet->GetBalance() || !masternodeSync.IsSynced() ) {
+            while (chainActive.Tip()->nTime < 1471482000 || vNodes.empty() || pwallet->IsLocked() || !fMintableCoins || nReserved >= pwallet->GetBalance() || !masternodeSync.IsSynced() ) {
 			//DEBUG!!!
-            while (chainActive.Tip()->nTime < 1471482000 || vNodes.empty() || pwallet->IsLocked() || !fMintableCoins || nReserveBalance >= pwallet->GetBalance() ) {
+            //while (chainActive.Tip()->nTime < 1471482000 || vNodes.empty() || pwallet->IsLocked() || !fMintableCoins || nReserveBalance >= pwallet->GetBalance() ) {
                 nLastCoinStakeSearchInterval = 0;
                 //LogPrintf("Miner: ProofOfStake is true, but wallet is not synces, locked, etc...\n");
                 MilliSleep(5000);
